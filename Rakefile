e20d04000f579753af6ba9d8787f11fa95b0779a
Lorenzo Planas <lplanas@qindio.com>
2010-07-10 17:55:34 +0200

    First commit

--- Rakefile
+++ Rakefile
@@ -10,37 +10,12 @@ begin
     gem.email = "lplanas@qindio.com"
     gem.homepage = "http://github.com/lplanas/sinatra-charter"
     gem.authors = ["Lorenzo Planas"]
-    gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
     # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
   end
   Jeweler::GemcutterTasks.new
 rescue LoadError
   puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
 end
-
-require 'rake/testtask'
-Rake::TestTask.new(:test) do |test|
-  test.libs << 'lib' << 'test'
-  test.pattern = 'test/**/test_*.rb'
-  test.verbose = true
-end
-
-begin
-  require 'rcov/rcovtask'
-  Rcov::RcovTask.new do |test|
-    test.libs << 'test'
-    test.pattern = 'test/**/test_*.rb'
-    test.verbose = true
-  end
-rescue LoadError
-  task :rcov do
-    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
-  end
-end
-
-task :test => :check_dependencies
-
-task :default => :test
 
 require 'rake/rdoctask'
 Rake::RDocTask.new do |rdoc|
---
+++ lib/sinatra-charter.rb
@@ -0,0 +1,59 @@
+# encoding: UTF-8
+require 'sinatra/base'
+require 'json'
+require 'gruff'
+
+module Sinatra
+  module Charter
+    module Helpers
+      def send_chart_url(options = {})
+        content :json
+
+        if options[:from] == 'json'
+          @chart = JSON.parse request.body.read.to_s 
+        else
+          @chart = params[:chart]
+        end
+
+        if File.exists? chart_path
+          chart_url
+        else
+          self.send "render_#{@chart['kind']}_chart"
+          chart_url
+        end
+      end
+
+      def chart_path
+        @chart_path ||= "public/charts/#{Digest::MD5.hexdigest @chart.to_s}.png"
+      end
+
+      def chart_url
+        hostname = /^http:\/\/([\w\.]+).*/.match(request.url)[1]
+        @chart_url ||= {"chart_url" => "http://#{hostname}/charts/#{chart_path}"}.to_json
+      end
+
+      def chart_theme
+        if @chart['theme']
+          {
+            :colors =>            (@chart['theme']['colors']            || ['#6cb12f']),
+            :marker_color =>      (@chart['theme']['marker_color']      || '#000'),
+            :font_color =>        (@chart['theme']['font_color']        || '#666'),
+            :background_colors => (@chart['theme']['background_colors'] || ['#fff', '#fff'])
+          }
+        else
+          { :colors => ['#6cb12f'], :marker_color => '#000', :font_color => '#666', :background_colors => ['#fff', '#fff'] }
+        end
+      end
+
+      def render_bar_chart
+        g = Gruff::Bar.new(@chart['size'].to_i)
+        g.theme = chart_theme
+        g.title = @chart['title']
+        g.labels = @chart['labels']
+        @chart['data'].each { |i| g.data i.first.to_i, i.last }
+        g.write chart_path
+      end
+    end
+  end
+  helpers Charter::Helpers
+end
